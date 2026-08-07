19d10a23a54961aa3122cd826e1bfe8a5e9f61ba
Iago <iagogcampos09@gmail.com>
2026-08-07 12:44:12 -0300

    Melhorando o código

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,15 +1,21 @@
 import { Text, View, Image, Pressable } from 'react-native';
 import styles from './styles';
 import { useNavigation } from "@react-navigation/native";
+import React, { useEffect} from 'react';
 
 export default function Home() {
     const navigation = useNavigation();
+    
+    useEffect(() => {
+      const timer = setTimeout (() => {
+        navigation.replace("Welcome");
+      }, 3000); //Espera 3 segundos
 
+      return () => clearTimeout(timer); //Limpa o timer se sair da tela
+    }, [navigation]);
   return (
-    <Pressable style={styles.container} onPress={() => navigation.replace('Welcome')}>
-    <View>
+    <View style={styles.container}>
       <Image source={require('../../assets/logotipo.png')} style={styles.logo} />
     </View>
-    </Pressable>
   );
 }
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -2,9 +2,22 @@ import { Text, View, Image, TextInput, TouchableOpacity, Pressable } from 'react
 import styles from './styles';
 import { useNavigation } from "@react-navigation/native";
 import { Checkbox } from 'expo-checkbox';
+import React, { useState } from 'react';
 
 export default function Login() {
     const navigation = useNavigation();
+    const [email, setEmail] = useState("");
+    const [senha,setSenha] = useState("");
+    const [ativo, setAtivo] = useState(true);
+
+    function verificacao(){
+        if(email != "adm" && senha != "1234"){
+            //Mensagem ruim
+        }else{
+            // boa
+        }
+
+    }
 
     return (
         <View style={styles.container}>
@@ -16,10 +29,21 @@ export default function Login() {
             <Text style={styles.subtitulo}>com E-mail e senha</Text>
 
             <Text style={styles.subtitulo2}>E-mail</Text>
-            <TextInput style={styles.input} placeholder="Digite seu e-mail" />
+            <TextInput 
+            style={styles.input} 
+            placeholder="Digite seu e-mail"
+            maxLength={50}
+            onChangeText={setEmail}
+            value={email} />
 
             <Text style={styles.subtitulo2}>Senha</Text>
-            <TextInput style={styles.input} placeholder="Digite sua senha" secureTextEntry />
+            <TextInput 
+            style={styles.input} 
+            placeholder="Digite sua senha" 
+            secureTextEntry
+            maxLength={20}
+            onChangeText={setSenha}
+            value={senha} />
 
             <View style={styles.checkboxContainer}>
                 <Checkbox
